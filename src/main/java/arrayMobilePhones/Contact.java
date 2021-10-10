bdea8cec7b5276d49302085445832da08533e82a
alexandrucreanga1 <alexandru.creanga1@gmail.com>
2021-10-10 14:02:49 +0300

    additional

--- src/main/java/arrayMobilePhones/Contact.java
+++ src/main/java/arrayMobilePhones/Contact.java
@@ -4,7 +4,7 @@ public class Contact {
     private String name;
     private String phoneNumber;
 
-    public Contact(String name, String phonenumber) {
+    public Contact(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
